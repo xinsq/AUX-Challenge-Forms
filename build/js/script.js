6161bb32195d6ddc757b4562bf779a5d41b0f133
xinsq <mail@xin-squared.com>
2013-01-29 09:19:18 -0500

    Initial javascript

--- build/js/script.js
+++ build/js/script.js
@@ -1,54 +1,43 @@
-// (function($,W,D)
-// {
-//     var JQUERY4U = {};
+$(document).ready(function(){
 
-//     JQUERY4U.UTIL =
-//     {
-//         setupFormValidation: function()
-//         {
-//             //form validation rules
-//             $("#signUp").validate({
-//                 rules: {
-//                     title: "required",
-//                     address: "required",
-//                     name: "required",
-//                     email: {
-//                         required: true,
-//                         email: true
-//                     },
-//                     password: {
-//                         required: true,
-//                         minlength: 5
-//                     },
-//                     cardNumber: "required",
-//                     security: "required",
-//                     month: "required",
-//                     year: "required"
-//                 },
-//                 messages: {
-//                     title: "Give your portfolio a name",
-//                     address: "Give a portfolio addres"
-//                     name: "Please enter your name",
-//                     email: "Please enter a valid email address",
-//                     password: {
-//                         required: "Please provide a password",
-//                         minlength: "Your password must be at least 5 characters long"
-//                     },
-//                     cardNumber: "Please enter a credit card number",
-//                     security: "Please enter your credit card's security code",
-//                     month: "Select a month",
-//                     year: "Select a year"
-//                 },
-//                 submitHandler: function(form) {
-//                     form.submit();
-//                 }
-//             });
-//         }
-//     }
+function returnCardType() {
 
-//     //when the dom has loaded setup form validation rules
-//     $(D).ready(function($) {
-//         JQUERY4U.UTIL.setupFormValidation();
-//     });
+	// Get credit card value and strip all non-numerical characters
+	var cardNumber = $('#cardNumber').val().replace(/\D/g,'');
+	var type = "none";
 
-// })(jQuery, window, document);
+	console.log(cardNumber);
+
+	// Use regular expressions to figure out card type
+	if (/^5[1-5]/.test(cardNumber)) {
+		type = "mastercard";
+	}
+	else if (/^4/.test(cardNumber)) {
+		type = "visa";
+	}
+	else if (/^3[47]/.test(cardNumber)) {
+		type = "americanexpress";
+	}
+	else if (/^6/.test(cardNumber)) {
+		type = "discover";
+	}
+	console.log(type);
+
+}
+
+// if (type == "mastercard") {
+
+$('#cardNumberLi::after').css('background', 'url("../images/sprite.png") no-repeat 0 -100px;');
+
+// 	// #cardNumberLi::after {
+// 	// background: url('../images/sprite.png') no-repeat 0 -122px;
+// 	// }
+
+// Visa card numbers start with a 4
+// MasterCard start with 51 through 55 and have 16 digits
+// American Express start with 34 or 37 and have 15 digits
+// Discover start with 6011 or 65. All have 16 digits
+
+//$('#cardNumber').bind('keyup', returnCardType);
+
+});
