f381d1d3b4f3fad08c1183aeee39cfa3b4c87940
xinsq <mail@xin-squared.com>
2013-01-29 11:02:39 -0500

    javascript credit card detection

--- build/js/script.js
+++ build/js/script.js
@@ -1,43 +1,41 @@
 $(document).ready(function(){
 
-function returnCardType() {
+var type = "none";
+
+function displayCardType() {
 
 	// Get credit card value and strip all non-numerical characters
 	var cardNumber = $('#cardNumber').val().replace(/\D/g,'');
-	var type = "none";
 
-	console.log(cardNumber);
+	$('#creditCardImg').css({'background-position':'0 -122px'});
+	// Visa card numbers start with a 4
+	// MasterCard start with 51 through 55 and have 16 digits
+	// American Express start with 34 or 37 and have 15 digits
+	// Discover start with 6011 or 65. All have 16 digits
 
-	// Use regular expressions to figure out card type
 	if (/^5[1-5]/.test(cardNumber)) {
 		type = "mastercard";
+		$('#creditCardImg').css({'background-position':'0 -290px'});
 	}
 	else if (/^4/.test(cardNumber)) {
 		type = "visa";
+		$('#creditCardImg').css({'background-position':'0 -206px'});
 	}
 	else if (/^3[47]/.test(cardNumber)) {
 		type = "americanexpress";
+		$('#creditCardImg').css({'background-position':'0 -164px'});
 	}
 	else if (/^6/.test(cardNumber)) {
 		type = "discover";
+		$('#creditCardImg').css({'background-position':'0 -248px'});
 	}
-	console.log(type);
 
+	else {
+		return;
+	}
 }
 
-// if (type == "mastercard") {
-
-$('#cardNumberLi::after').css('background', 'url("../images/sprite.png") no-repeat 0 -100px;');
-
-// 	// #cardNumberLi::after {
-// 	// background: url('../images/sprite.png') no-repeat 0 -122px;
-// 	// }
-
-// Visa card numbers start with a 4
-// MasterCard start with 51 through 55 and have 16 digits
-// American Express start with 34 or 37 and have 15 digits
-// Discover start with 6011 or 65. All have 16 digits
-
-//$('#cardNumber').bind('keyup', returnCardType);
+$('#cardNumber').bind('keyup', displayCardType);
+$('#card')
 
 });
