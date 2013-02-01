017186c29573f8b2e2e129618f5bb710ecf498bf
xinsq <mail@xin-squared.com>
2013-02-01 09:16:08 -0500

    Set attribute checked

--- build/js/script.js
+++ build/js/script.js
@@ -4,36 +4,39 @@
 	// Get credit card value and strip all non-numerical characters
 	var cardNumber = document.getElementById('cardNumber').value.replace(/\D/g,'');
 
-	// Reset position values
-	document.getElementById('amexLabel').style.backgroundPosition='0 -206px';
-	document.getElementById('visaLabel').style.backgroundPosition='-48px -164px';
-	document.getElementById('discoverLabel').style.backgroundPosition='-96px -164px';
-	document.getElementById('mastercardLabel').style.backgroundPosition='-144px -164px';
-	document.getElementById('securityImg').style.backgroundPosition='0 -80px';
+
 
 	// MasterCard start with 51 through 55 and have 16 digits
 	if (/^5[1-5]/.test(cardNumber)) {
-		document.getElementById('mastercardLabel').style.backgroundPosition='-144px -122px';
+		document.getElementById("mastercard").setAttribute("checked");
 		document.getElementById('securityImg').style.backgroundPosition='0 -374px';
 
 	}
 
 	// Visa card numbers start with a 4
 	else if (/^4/.test(cardNumber)) {
-		document.getElementById('visaLabel').style.backgroundPosition='-48px -122px';
+		document.getElementById("visa").setAttribute("checked");
 		document.getElementById('securityImg').style.backgroundPosition='0 -374px';
 	}
 
 	// American Express start with 34 or 37 and have 15 digits
 	else if (/^3[47]/.test(cardNumber)) {
-		document.getElementById('amexLabel').style.backgroundPosition='0 -122px';
+		document.getElementById("amex").setAttribute("checked");
 		document.getElementById('securityImg').style.backgroundPosition='0 -331px';
 	}
 
 	// Discover start with 6011 or 65. All have 16 digits
 	else if (/^6/.test(cardNumber)) {
-		document.getElementById('discoverLabel').style.backgroundPosition='-96px -122px';
+		document.getElementById("discover").setAttribute("checked");
 		document.getElementById('securityImg').style.backgroundPosition='0 -374px';
+	}
+
+	else {
+		document.getElementById("amex").setAttribute("unchecked");
+		document.getElementById("visa").setAttribute("unchecked");
+		document.getElementById("mastercard").setAttribute("unchecked");
+		document.getElementById("discover").setAttribute("unchecked");
+		document.getElementById('securityImg').style.backgroundPosition='0 -80px';
 	}
 }
 
