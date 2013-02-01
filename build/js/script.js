21e66df8837706822a16a4b3eea0ea04030c3353
xinsq <mail@xin-squared.com>
2013-02-01 10:42:05 -0500

    javascript fix

--- build/js/script.js
+++ build/js/script.js
@@ -1,50 +1,64 @@
 (function () {
 
-	function displayCardType() {
-	// Get credit card value and strip all non-numerical characters
-	var cardNumber = document.getElementById('cardNumber').value.replace(/\D/g,'');
+function getCardType(accountNumber) {
+  var result = "unknown";
 
+  if (/^5[1-5]/.test(accountNumber)) // MasterCard start with 51 through 55 and have 16 digits
+  { result = "mastercard";}
 
+  else if (/^4/.test(accountNumber)) // Visa card numbers start with a 4
+  { result = "visa";}
 
-	// MasterCard start with 51 through 55 and have 16 digits
-	if (/^5[1-5]/.test(cardNumber)) {
-		document.getElementById("mastercard").setAttribute("checked");
+  else if (/^3[47]/.test(accountNumber)) // American Express start with 34 or 37 and have 15 digits
+  { result = "amex";}
+
+  else if (/^6/.test(accountNumber)) // Discover start with 6011 or 65. All have 16 digits
+  { result = "discover";}
+
+  return result;
+}
+
+function setCardType(event) {
+	var value = event.target.value,
+		type = getCardType(value);
+
+	switch (type)
+	{
+		case "mastercard":
+		document.getElementById("mastercard").checked="true";
 		document.getElementById('securityImg').style.backgroundPosition='0 -374px';
+        break;
 
-	}
-
-	// Visa card numbers start with a 4
-	else if (/^4/.test(cardNumber)) {
-		document.getElementById("visa").setAttribute("checked");
+    	case "visa":
+		document.getElementById("visa").checked="true";
 		document.getElementById('securityImg').style.backgroundPosition='0 -374px';
-	}
+        break;
 
-	// American Express start with 34 or 37 and have 15 digits
-	else if (/^3[47]/.test(cardNumber)) {
-		document.getElementById("amex").setAttribute("checked");
+    	case "discover":
+        document.getElementById("discover").checked="true";
+		document.getElementById('securityImg').style.backgroundPosition='0 -374px';
+        break;
+
+        case "amex":
+        document.getElementById("amex").checked="true";
 		document.getElementById('securityImg').style.backgroundPosition='0 -331px';
-	}
+        break;
 
-	// Discover start with 6011 or 65. All have 16 digits
-	else if (/^6/.test(cardNumber)) {
-		document.getElementById("discover").setAttribute("checked");
-		document.getElementById('securityImg').style.backgroundPosition='0 -374px';
-	}
-
-	else {
-		document.getElementById("amex").setAttribute("unchecked");
-		document.getElementById("visa").setAttribute("unchecked");
-		document.getElementById("mastercard").setAttribute("unchecked");
-		document.getElementById("discover").setAttribute("unchecked");
-		document.getElementById('securityImg').style.backgroundPosition='0 -80px';
+   		default:
+   		document.getElementById("mastercard").checked="false";
+   		document.getElementById("visa").checked="false";
+   		document.getElementById("discover").checked="false";
+   		document.getElementById("amex").checked="false";
+   		document.getElementById('securityImg').style.backgroundPosition='0 -80px';
 	}
 }
 
-document.getElementById('cardNumber').addEventListener('keyup', displayCardType);
+document.getElementById('cardNumber').addEventListener('keyup', setCardType);
+document.getElementById('cardNumber').addEventListener('blur', setCardType);
 
 Modernizr.load({
 	test: Modernizr.input.required,
-	nope: 'parsley.min.js'
+	nope: 'parsley-standalone.min.js'
 });
 
 })();
